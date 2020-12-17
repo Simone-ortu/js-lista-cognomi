d3022aa37f7a12c4603b1f0a9db9fe9bcc1b3974
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-17 15:29:38 +0100

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@
 // Scrivi anche la posizione della lista in cui il
 // nuovo utente si trova.
 
-
+// variabili
 var cognome = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 var cognomeUtente = prompt('Inserire cognome');
+
+// Maiuscola al cognome inserito
+
 var cognomeCap = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
- console.log(cognomeCap);
- cognome.push(cognomeCap);
+console.log(cognomeCap);
+
+// Aggiunta all'arroy la nostra variabile
+ cognome[cognome.length]= cognomeCap;
  console.log(cognome);
  cognome.sort();
- console.log(cognomeCap.length);
 
-
-
+// stampa dell'arroy in ordine alfabetico
+ for (let i = 0; i < cognome.length; i++) {
+    console.log(cognome[i])
+  }
+//  stampa dell'ordine dellaa nostra variabile nell'arroy
+  console.log(cognome.indexOf(cognomeCap));
 
